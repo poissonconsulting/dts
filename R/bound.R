2b8dcb60e9e2a758512a9872d0604bf3da32f4e4
Joe Thorley <joe@poissonconsulting.ca>
2018-09-30 10:30:59 -0700

    add adjust to bound

--- R/bound.R
+++ R/bound.R
@@ -4,6 +4,8 @@
 #' 
 #' @inheritParams check_dts
 #' @param bounds A sorted double vector of the lower and upper permitted bounds.
+#' @param adjust A flag indicating whether to adjust values outside the bounds to the permitted bounds
+#'  or whether to set to NA (the default).
 #'
 #' @return A data frame
 #' @export
@@ -11,12 +13,18 @@
 #' @examples
 #' dts_bound(dts_data[1:5,], bounds = c(0,Inf))
 dts_bound <- function(x, date_time = "DateTime", value = "Value",
-                      bounds = c(-Inf, Inf)) {
+                      bounds = c(-Inf, Inf), adjust = FALSE) {
   check_dts(x, date_time = date_time, value = value)
-  check_vector(bounds, c(-Inf, Inf), length = 2L, unique = TRUE)
+  check_vector(bounds, c(-Inf, Inf), length = 2L, sorted = TRUE)
   check_sorted(bounds)
+  check_flag(adjust)
   
-  x[[value]][x[[value]] < bounds[1]] <- NA_real_
-  x[[value]][x[[value]] > bounds[2]] <- NA_real_
+  if(!adjust) {
+    x[[value]][x[[value]] < bounds[1]] <- NA_real_
+    x[[value]][x[[value]] > bounds[2]] <- NA_real_
+  } else {
+    x[[value]][x[[value]] < bounds[1]] <- bounds[1]
+    x[[value]][x[[value]] > bounds[2]] <- bounds[2]
+  }
   x
 }
--- tests/testthat/test-bound.R
+++ tests/testthat/test-bound.R
@@ -2,5 +2,7 @@ context("bound")
 
 test_that("bound", {
   expect_equal(dts_bound(dts_data[1:5,], bound = c(0, Inf))$Value, 
-                   c(NA, NA, NA, NA, 10.26348), tolerance = 1e-06)
+               c(NA, NA, NA, NA, 10.26348), tolerance = 1e-06)
+  expect_equal(dts_bound(dts_data[1:5,], bound = c(0, Inf), adjust = TRUE)$Value, 
+               c(0, NA, NA, NA, 10.26348), tolerance = 1e-06)
 })
